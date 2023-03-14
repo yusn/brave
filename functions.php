a4dab2eb9b8af0f887e533f1f6a4523771f9baec
Yusn <csslog@gmail.com>
2023-03-14 21:40:57 +0800

    PATCH: 修改城市标记样式

--- functions.php
+++ functions.php
@@ -445,10 +445,10 @@ function get_brave_post_meta() {
 	$html_string = '';
 	if (isset($geo_latitude) && isset($geo_longitude) && isset($geo_public)) {
 		$local = $geo_public === '1' ? 'i-local' : 'i-pin';
-		if ($geo_public === '1' || current_user_can('manage_options')) {
+		if ($geo_public === '1' || current_user_can('administrator')) {
 			$html_string .= '<span class="ml"><a class="gmap" href="//maps.google.com/maps?q=$geo_latitude,$geo_longitude&hl=zh-cn&t=m&z=15" itemprop="map" itemtype="//schema.org/Place"><span class="' . $local . '"></span></a></span>';
 			if (isset($geo_city)) {
-				$html_string .= '<span> ' . $geo_city . '</span>';
+				$html_string .= '<span class="ml-tiny font-tiny">' . $geo_city . '</span>';
 			}
 		} elseif ($geo_public === '0') {
 				$html_string .= '<span class="' . $local . ' ml" itemprop="map" itemtype="//schema.org/Place"></span>';
