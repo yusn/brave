27c3ff6d4da1a72c7fdf622057ab4e8d818cfb0a
Yusn <csslog@gmail.com>
2023-03-16 20:04:05 +0800

    PATCH: 修改变量

--- footer.php
+++ footer.php
@@ -4,17 +4,17 @@
 		<div class="f-name">
 			<?php 
 				// 获取变量 $logo, $beian
-				extract(pick_array(get_brave_config('basic'), ['logo', 'beian']));
+				extract(pick_array(get_brave_config('basic'), ['logo', 'beian', 'site_name']));
 				if (!empty($logo)) :
 			?>
-				<a id="logo" href="/" title="<?php get_bloginfo('name'); ?>">
+				<a id="logo" href="/" title="<?php echo $site_name; ?>">
 					<img style="display:inline-block;max-width:60px;margin: 0 auto" src= "<?php echo $logo ?>">
 				</a>
 			<?php else : ?>
 				<span class="logo i-frog left"></span>
 			<?php endif; ?>
 		</div>
-		<div class="f-items mt5">&copy;<?php echo date("Y"); ?> <?php echo get_bloginfo('name'); ?> Some Rights Reserved
+		<div class="f-items mt5">&copy;<?php echo date("Y"); ?> <?php echo $site_name; ?> Some Rights Reserved
 			<div class="mt5"><a href="//github.com/yusn/Brave" class="mr">Brave</a><a href="https://beian.miit.gov.cn"><?php echo $beian; ?></a></div>
         </div><!-- .f-items -->
 	</div><!-- .footerin -->
