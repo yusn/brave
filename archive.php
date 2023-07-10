21d1bc4ee4a41b59a4aad88474ef8d2ff3d8b881
Yusn <csslog@gmail.com>
2023-07-11 07:21:07 +0800

    修复 archive 页分页无效问题

--- archive.php
+++ archive.php
@@ -1,5 +1,5 @@
 <?php get_header(); ?>
-	<section class="container clear" role="main">
+	<div class="clear" role="main" itemscope itemtype="//schema.org/ArchivePage">
 		<?php if ( have_posts() ) : ?>
 			<header class="search-meta pt20 pb20 mb20 clear">
 				<div class="inner">
@@ -9,16 +9,16 @@
 					?>
 				</div>
 			</header>
-		<div class="archive-list">
-		<?php
-			while ( have_posts() ) : the_post();
-				get_template_part( 'template-parts/content', 'search');
-			endwhile;
-			get_brave_content_nav( 'next' );
-		?>
-		</div>
+			<div class="container content archive-list mt20">
+			<?php
+				while ( have_posts() ) : the_post();
+					get_template_part( 'template-parts/content', 'search');
+				endwhile;
+				get_brave_content_nav( 'next' );
+			?>
+			</div>
 		<?php else : ?>
 			<?php get_template_part( 'content', 'none' ); ?>
 		<?php endif; ?>
-	</section>
+	</div>
 <?php get_footer(); ?>
